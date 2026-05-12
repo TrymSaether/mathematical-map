46b3e8ed9a17509f12865a3e2fa22ec2a404ea99
Trym Saether <70568409+TrymSaether@users.noreply.github.com>
2026-05-12 12:53:03 +0200

    Apply theme attributes at app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
 import { Sidebar } from "./components/Sidebar";
 import { TopBar } from "./components/TopBar";
 import { GraphCanvas } from "./components/GraphCanvas";
 import { NodePanel } from "./components/NodePanel";
 import { CommandPalette } from "./components/CommandPalette";
 import { useKeyboardNav } from "./hooks/useKeyboardNav";
+import { useStore } from "./store";
 
 export default function App() {
   useKeyboardNav();
+  const themeId = useStore((s) => s.themeId);
+  const colorMode = useStore((s) => s.colorMode);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.dataset.theme = themeId;
+    root.dataset.colorMode = colorMode;
+  }, [themeId, colorMode]);
 
   return (
     <div className="atlas-shell flex h-screen w-screen flex-col overflow-hidden">
